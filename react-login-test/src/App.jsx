32facc39b68c6e1a4c57901c8727e41b0edeee3c
jorge9372 <jorge9372@gmail.com>
2023-09-17 18:06:05 -0400

    feat: login auth0, gitHub autentication

---
+++ react-login-test/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton/LogoutButton';
+import Profile from './components/Profile';
+
+import { useAuth0 } from '@auth0/auth0-react';
+
+function App() {
+	const { isAuthenticated, isLoading } = useAuth0();
+
+	if (isLoading) return <h1>Loading...</h1>;
+
+	return (
+		<div>
+			<h1>Application</h1>
+			{isAuthenticated ? <LogoutButton /> : <LoginButton />}
+
+			<Profile />
+		</div>
+	);
+}
+
+export default App;
---
+++ react-login-test/src/components/Profile/Profile.jsx
@@ -0,0 +1,20 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import JSONPretty from 'react-json-pretty';
+import 'react-json-pretty/themes/monikai.css';
+
+const Profile = () => {
+	const { user, isAuthenticated } = useAuth0();
+	return (
+		isAuthenticated && (
+			<div>
+				<h2>Profile</h2>
+				<img width={150} src={user.picture} alt={user.name} />
+				<p>{user.name}</p>
+				<p>{user.nickname}</p>
+				<JSONPretty id='json-pretty' data={user} />
+			</div>
+		)
+	);
+};
+
+export default Profile;
